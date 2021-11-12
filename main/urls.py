e57cae9fdc8e769cbfcbc4708f2e9695087786bc
BohdanMukhailiuk <bohdanmykhailiuk2001@gmail.com>
2021-11-12 11:46:24 +0200

    created logout url

--- main/urls.py
+++ main/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('teachers', views.teachers, name="teachers"),
     path('register', views.register, name="register"),
     path('login', views.CustomLoginView.as_view(), name="login"),
+    path('logout', views.logout_request, name="logout"),
 
 ]
