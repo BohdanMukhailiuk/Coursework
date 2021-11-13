d1b922ecb862f8c90b11d65bb59c490796e3df7e
BohdanMukhailiuk <bohdanmykhailiuk2001@gmail.com>
2021-11-14 00:34:46 +0200

    created url to add and views comment to selected teacher

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 
     path('', views.index, name="home"),
     path('about', views.about, name="about"),
+    path('about/<int:pk>/', views.about, name="about"),
+    path('comment/<int:pk>/', views.comment, name="comment"),
     path('comment', views.comment, name="comment"),
     path('order', views.order, name="order"),
     path('teachers', views.teachers, name="teachers"),
