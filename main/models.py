c9e3f2edb7b9ac2ce5917f3d0ae8ad2bf472028f
BohdanMukhailiuk <bohdanmykhailiuk2001@gmail.com>
2021-11-16 11:04:57 +0200

    changed subject in Select_Subject and added choose subject field to Task models

--- main/models.py
+++ main/models.py
@@ -2,13 +2,25 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+Select_Subject = (
+    ('Математика', 'Математика'),
+    ('Англійська мова', 'Англійська мова'),
+    ('Українська мова', 'Українська мова'),
+    ('Фізика', 'Фізика'),
+    ('Хімія', 'Хімія'),
+    ('Французька', 'Французька'),
+
+)
+
+
 class Task(models.Model):
     image = models.ImageField(verbose_name='Зображення', blank=True)
     last_name = models.CharField(verbose_name="Прізвище", max_length=50)
     first_name = models.CharField(verbose_name="Ім'я", max_length=50)
     surname = models.CharField(verbose_name="По батькові", max_length=50)
-    subject = models.CharField(verbose_name="Назва предмету викладання", max_length=50)
-    price_per_hour = models.CharField(verbose_name="Ціна заняття за годину", max_length=50)
+    choose_subject = models.CharField(verbose_name="Назва предмету викладання", max_length=16, choices=Select_Subject,
+                                      null=True)
+    price_per_hour = models.CharField(verbose_name="Ціна заняття за годину у грн", max_length=50)
     specialization = models.CharField(verbose_name="Напрямок викладання", max_length=50)
     experience = models.CharField(verbose_name="Стаж роботи", max_length=50)
     extra_information = models.TextField(verbose_name="Додаткова інформація")
@@ -16,7 +28,7 @@ class Task(models.Model):
     email = models.EmailField(verbose_name="Електронна пошта", max_length=254, null=True)
 
     def __str__(self):
-        return "{} {} {} {} {}".format(self.last_name, self.first_name, self.surname, self.subject, self.price_per_hour, self.image)
+        return "{} {} {} {} {}".format(self.last_name, self.first_name, self.surname, self.price_per_hour, self.image)
 
     class Meta:
         verbose_name = 'Репетитор'
